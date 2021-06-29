2b9ab74342182ae5ec535e8b74fc6b5bb33db683
Poorvid <poorvid61@gmail.com>
2021-06-29 10:53:58 +0530

    Update firstTest.java

--- src/test/java/udemyappium_1/firstTest.java
+++ src/test/java/udemyappium_1/firstTest.java
@@ -29,7 +29,7 @@ public class firstTest {
 		
 		
 			
-	Runtime.getRuntime().exec("$WORKSPACE\\src\\main\\resources\\startemulator.bat");
+	Runtime.getRuntime().exec("${WORKSPACE}\\src\\main\\resources\\startemulator.bat");
 	
 	 Runtime runtime = Runtime.getRuntime();
 	    try {
