233f22926b1b8690f514c812ee4bb6ed3f094ece
Poorvid <poorvid61@gmail.com>
2021-06-29 10:33:35 +0530

    Update firstTest.java

--- src/test/java/udemyappium_1/firstTest.java
+++ src/test/java/udemyappium_1/firstTest.java
@@ -16,6 +16,7 @@ import org.testng.annotations.Test;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import java.util.concurrent.TimeUnit;
 
 public class firstTest {
 
